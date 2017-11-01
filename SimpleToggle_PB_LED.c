2c4f27e1fbb5bf6b6342d5b6239e24d4b86544d6
Jmichelle1 <31673374+Jmichelle1@users.noreply.github.com>
2017-11-01 11:24:31 -0400

    Created file

---
+++ SimpleToggle_PB_LED.c
@@ -0,0 +1,32 @@
+  /* SimpleToggle_PB_LED */
+
+const byte LEDpin = 3;
+const byte SWITCHpin = 2;
+
+byte LEDstate = LOW;
+byte SWITCHstate;
+byte lastSWITCHstate = HIGH;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LEDpin, OUTPUT);
+pinMode(SWITCHpin, INPUT);
+// turn on pullup resistor
+digitalWrite(SWITCHpin, HIGH);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+SWITCHstate = digitalRead(SWITCHpin);
+
+if ((lastSWITCHstate == HIGH) && (SWITCHstate == LOW))
+{
+  //TURNS LIGHT ON AND OFF
+    LEDstate = !LEDstate;
+    digitalWrite(LEDpin, LEDstate);
+    
+}
+lastSWITCHstate = SWITCHstate;
+}
